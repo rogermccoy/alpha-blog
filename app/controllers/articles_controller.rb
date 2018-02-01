e4e817740bb882a6e78f121ff84d4f4f7d630d23
rogermccoy@gmail.com <rogermccoy@gmail.com>
2018-02-01 01:55:38 +0000

    Stuff bad commit message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,37 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
-  def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+  def index
+    @articles = Article.all()
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
   
   private 
     def article_params
