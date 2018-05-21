ffacd9ea44e75f3344b69a527441c31a83b6cd7a
rogermccoy@gmail.com <rogermccoy@gmail.com>
2018-05-21 03:26:48 +0000

    Add admin user functinality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,13 @@ class UsersController < ApplicationController
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
 
   private
   def set_user
@@ -45,7 +52,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
       end
